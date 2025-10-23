ee58d028a1fba704fea22df4e3ff758ec609b518
Saet1a <joaorogeroliveira@gmail.com>
2025-10-23 15:43:03 -0300

    feat: Read prompt from command-line argument

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import sys
+
 
 
 
@@ -9,9 +11,17 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
+    arguments = sys.argv[1:]
+    if not arguments:
+        print("Please provide a prompt as a command line argument.")
+        sys.exit(1)
+
+    prompt = " ".join(arguments)
+
     response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    model='gemini-2.0-flash-001', contents=prompt
     )
+
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
