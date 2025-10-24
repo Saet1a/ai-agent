b0218a7b0e54008156686213e6e0151bbd8bf77d
Saet1a <joaorogeroliveira@gmail.com>
2025-10-23 21:10:14 -0300

    feat: Implement --verbose flag to toggle detailed console output

--- main.py
+++ main.py
@@ -12,26 +12,37 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
-    arguments = sys.argv[1:]
+    verbose = "--verbose" in sys.argv
+    arguments = []
+
+    for arg in sys.argv[1:]:
+        if not arg.startswith("--"):
+            arguments.append(arg)    
+
     if not arguments:
         print("Please provide a prompt as a command line argument.")
         sys.exit(1)
 
     prompt = " ".join(arguments)
+
+    if verbose:
+        print(f"User prompt: {prompt}\n")
+
     messages = [
     types.Content(role="user", parts=[types.Part(text=prompt)]),]
 
-    generate_content(client,messages)
+    generate_content(client,messages,verbose)
 
 
-def generate_content(client, messages):
+def generate_content(client,messages,verbose):
     response = client.models.generate_content(
         model='gemini-2.0-flash-001',
         contents=messages,
     )
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if verbose:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":
