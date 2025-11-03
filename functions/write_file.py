4102b34c2ce2c532a9dcc84031d339b535c18bd9
Saet1a <joaorogeroliveira@gmail.com>
2025-11-02 23:05:26 -0300

    feat: Define tool schemas for read, write, and execute functions

--- functions/write_file.py
+++ functions/write_file.py
@@ -1,6 +1,6 @@
 import os 
 from config import MAX_CHARS
-
+from google.genai import types
 
 
 
@@ -22,3 +22,22 @@ def write_file(working_directory, file_path, content):
         return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
     except Exception as e:
         return f"Error: {e}"
+    
+schema_write_file = types.FunctionDeclaration(
+    name="write_file",
+    description="Writes content to a specified file within the working directory, creating directories as needed.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "content": types.Schema(
+                type=types.Type.STRING,
+                description="The content to write to the file.",
+            ),
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path to the file to read, relative to the working directory.",
+            ),
+        },
+        required=["file_path", "content"],
+    ),
+)
--- tests.py
+++ tests.py
@@ -1,11 +1,19 @@
 from functions.run_python_file import run_python_file
 from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
+from functions.write_file import write_file
 
 def test():
-    result = get_files_info({'directory': '.'})
+    result = get_file_content({'file_path': 'main.py'})
+    print(result)
+
+    result = write_file({'file_path': 'main.txt', 'content': 'hello'})
     print(result)
 
     result = get_files_info({'directory': 'pkg'})
+    print(result)
+
+    result = run_python_file({'file_path': 'main.py'})      
     print(result)
 
 
